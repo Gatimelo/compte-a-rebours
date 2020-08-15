64cefebaf87c0b67cdd5219d64dd5280de706812
Gatimelo <48652274+Gatimelo@users.noreply.github.com>
2020-08-15 15:31:06 +1100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  consumer_key: "SNj0eGGOI02VvVXZOhnn2rApM",
+  consumer_secret: "FeZ8wS56f3u6iUG7LomxevpbOcWi9Yf7CUzpezLjRJIwC6XjrH",
+  access_token: "1161793568710828033-NklfVGt0UBuJijQxurVRNL40gFPefI",
+  access_token_secret: "0OuCzs5LXZKdmvlr18psgqJBPQR0fNXXiK6seRQhKMwrO"
+}
